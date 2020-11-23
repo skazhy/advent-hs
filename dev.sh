711d90d424ab7ee432db51bc94454f0429a033b0
Karlis Lauva <k@karlis.me>
2020-11-23 10:01:50 +0200

    move puzzle year into a var

--- dev.sh
+++ dev.sh
@@ -3,6 +3,7 @@
 set -e
 
 # Usage: ./dev.sh [day]
+YEAR=2020
 TODAY=$(date "+%d" | sed -e 's/0//g')
 DAY=${1:-"$TODAY"}
 
@@ -10,7 +11,7 @@ DAY=${1:-"$TODAY"}
 SRC_FILENAME="src/Day$DAY.hs"
 if [ ! -f "$SRC_FILENAME" ]; then
   echo "Creating new source file for day $DAY..."
-  TITLE=$(curl -s https://adventofcode.com/2020/day/$DAY | grep -m1 h2 | sed 's/.*--- \(Day .*\) ---.*/\1/')
+  TITLE=$(curl -s https://adventofcode.com/$YEAR/day/$DAY | grep -m1 h2 | sed 's/.*--- \(Day .*\) ---.*/\1/')
   cp src/Day0.hs src/Day$DAY.hs
   sed -i "" "s/Day0/Day$DAY/g; s/TODO/$TITLE/" $SRC_FILENAME
 
@@ -27,11 +28,9 @@ if [ ! -f "$INPUT_FILENAME" ]; then
     echo "Please save the cookie header value in .cookie!"
     exit 1
   fi
-  COOKIE=$(cat .cookie)
-  INPUT=$(curl -s --fail https://adventofcode.com/2019/day/$DAY/input -H "Cookie: $COOKIE")
-  echo "$INPUT" > $INPUT_FILENAME
+  curl -s --fail https://adventofcode.com/$YEAR/day/$DAY/input -H "Cookie: $(cat .cookie)" > "$INPUT_FILENAME"
   git add --intent-to-add $INPUT_FILENAME
 fi
 
-# Start GHCi with the new file loaded
+# Start GHCi with the selected source file loaded
 ghci -isrc $SRC_FILENAME

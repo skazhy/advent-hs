968b49436290130729711bb5d2054ab21e83c385
Karlis Lauva <k@karlis.me>
2020-12-02 17:30:00 +0200

    day 2

---
+++ src/Day2.hs
@@ -0,0 +1,48 @@
+{-|
+Module      : Day2
+Description : Day 2: Password Philosophy
+
+<https://adventofcode.com/2020/day/2>
+
+-}
+
+module Day2 where
+
+import Advent
+
+data Password =
+    Password { low :: Int
+             , high :: Int
+             , letter :: Char
+             , value :: String
+             } deriving (Show)
+
+parseIndex :: String -> Int
+parseIndex = abs . read
+
+fromString :: String -> Password
+fromString s =
+    let [i, l, v] = words s
+        indexes = break (== '-') i
+    in Password { low = parseIndex $ fst indexes
+                , high = parseIndex $ snd indexes
+                , letter = head l
+                , value = v
+                }
+
+isValidPassword :: Password -> Bool
+isValidPassword pw =
+    let count = length $ filter (== letter pw) (value pw)
+    in low pw <= count && count <= high pw
+
+charMatcher :: (Password -> Int) -> Password -> Bool
+charMatcher val pw = value pw !! (val pw - 1) == letter pw
+
+isValidPassword2 :: Password -> Bool
+isValidPassword2 pw =
+    charMatcher low pw /= charMatcher high pw
+
+main = do
+    input <- parsedInput 2 (map fromString . lines)
+    print $ length $ filter isValidPassword input
+    print $ length $ filter isValidPassword2 input

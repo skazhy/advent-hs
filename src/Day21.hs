2afc5694a2ca3144ebda869f308364b3af2a0860
Karlis Lauva <k@karlis.me>
2020-12-21 22:51:58 +0200

    day 21: rm redundant monad join

--- src/Day21.hs
+++ src/Day21.hs
@@ -9,7 +9,6 @@ module Day21 where
 
 import Advent
 
-import Control.Monad (join)
 import Data.Bifunctor (first, second)
 import Data.List (isPrefixOf, intersect, intercalate, repeat, (\\))
 import Data.Map (Map, empty, fromList, union, unionWith, elems)
@@ -25,15 +24,15 @@ allergenMap = fromList . uncurry (zipWith (flip (,))) . first repeat
 allergens :: [([String], [String])] -> Map String [String]
 allergens = foldl (unionWith intersect) empty . map allergenMap
 
-knownAllergens :: [([String], [String])] -> Map String [String]
+knownAllergens :: [([String], [String])] -> Map String String
 knownAllergens =
     go empty . allergens where
     go to from | null from = to
-               | otherwise = go (union to $ M.filter ((== 1) . length) from)
-                                (M.filter (not . null) $ fmap (\\ (join $ elems to)) from)
+               | otherwise = go (union to $ head <$> M.filter ((== 1) . length) from)
+                                (M.filter (not . null) $ fmap (\\ elems to) from)
 
 main = do
     input <- parsedInput 21 (map parseLine . lines)
     let known = knownAllergens input
-    print $ sum $ map (length . (\\ (join $ elems known)) . fst) input
-    print $ (intercalate "," . join . elems) known
+    print $ sum $ map (length . (\\ elems known) . fst) input
+    print $ (intercalate "," .  elems) known
